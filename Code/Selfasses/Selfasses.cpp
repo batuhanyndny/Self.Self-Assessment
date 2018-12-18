2076f92af7c3d27f242246e27bcf4df0e04e65b2
Batuhan Yenidünya <44680817+batuhanyndny@users.noreply.github.com>
2018-12-19 00:02:19 +0300

    change

--- Code/Selfasses/Selfasses.cpp
+++ Code/Selfasses/Selfasses.cpp
@@ -50,80 +50,80 @@ int main()
 	loadIntoArray(Extra,  ExtraFileName);
 	loadIntoArray(Listen, ListenFileName);
 
-	//srand(236112);
-	//int random = rand() % 7 + 1;
-	////cout << random;
+	srand(time(NULL));
+	int random = rand() % 7+1;
+	cout << random;
 
 
-	//string line;
-	//string rep = "Read" + to_string(random);
-	//fstream myfile("weeklyplan1.rtf");
-	//ofstream file2;
-	//file2.open("selfass.rtf");
-	//if (myfile.is_open())
-	//{
-	//	while (getline(myfile, line))
-	//	{
-
-	//		size_t pos = line.find(rep);
-	//		if (pos != string::npos)
-	//		{
-	//			line.replace(line.find(rep), rep.length(), Read.at(random-1));
-	//			file2 << line << "\n";
-	//		}
-	//		else
-	//		{
-	//			file2 << line << "\n";
-	//		}
-
-
-	//	}
-	//	myfile.close();
-	//	file2.close();
-	//}
-	//else
-	//{
-	//	cout << "Unable to open file";
-	//}
-	//string curline;
-	//fstream res("selfass.rtf");
-	//ofstream finalres;
-	//string rep2 = "Read";
-	//finalres.open("DrumRoll.rtf");
-	//if (res.is_open())
-	//{
-	//	while (getline(res, curline))
-	//	{
-	//		for (int i = 1; i <= 7; i++)
-	//		{
-	//			
-	//			string curstr = rep2 + to_string(i);
-	//			//cout << curstr << endl;
-	//			size_t pos2 = curline.find(curstr);
-	//			if (pos2 != string::npos)
-	//			{
-	//				curline.replace(curline.find(curstr), curstr.length(), "-");
-
-	//			}
-	//		}
-	//		finalres << curline << "\n";
-	//	}
-	//	res.close();
-	//	finalres.close();
-	//	
-	//}
-	//
 	string line;
-	fstream file("weeklyplan1.rtf");
-	if (file.is_open())
+	string rep = "Read" + to_string(random);
+	fstream myfile("weeklyplan1.rtf");
+	ofstream file2;
+	file2.open("selfass.rtf");
+	if (myfile.is_open())
 	{
-		while (getline(file, line))
+		while (getline(myfile, line))
 		{
-			cout << line << endl;
-		}
-	}
 
-	system("PAUSE");
+			size_t pos = line.find(rep);
+			if (pos != string::npos)
+			{
+				line.replace(line.find(rep), rep.length(), Read.at(random));
+				file2 << line << "\n";
+			}
+			else
+			{
+				file2 << line << "\n";
+			}
+
+
+		}
+		myfile.close();
+		file2.close();
+	}
+	else
+	{
+		cout << "Unable to open file";
+	}
+	string curline;
+	fstream res("selfass.rtf");
+	ofstream finalres;
+	string rep2 = "Read";
+	finalres.open("DrumRoll.rtf");
+	if (res.is_open())
+	{
+		while (getline(res, curline))
+		{
+			for (int i = 1; i <= 7; i++)
+			{
+				
+				string curstr = rep2 + to_string(i);
+				//cout << curstr << endl;
+				size_t pos2 = curline.find(curstr);
+				if (pos2 != string::npos)
+				{
+					curline.replace(curline.find(curstr), curstr.length(), "-");
+
+				}
+			}
+			finalres << curline << "\n";
+		}
+		res.close();
+		finalres.close();
+		
+	}
+	//
+	//string line;
+	//fstream file("weeklyplan1.rtf");
+	//if (file.is_open())
+	//{
+	//	while (getline(file, line))
+	//	{
+	//		cout << line << endl;
+	//	}
+	//}
+
+	//system("PAUSE");
 
 	return 0;
 }
